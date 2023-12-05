ccf74e51c6bf0fcd9a5ef718848e2b23d38ec598
ammaar-nizam <ammaar.nizam@outlook.com>
2023-12-06 00:04:10 +0530

    Added Sequelize ORM to the project and restructed

--- node-backend/src/routes/user.js
+++ node-backend/src/routes/user.js
@@ -1,4 +1,4 @@
-const User = require("../models/User");
+const User = require("../models1/User");
 const CryptoJS = require("crypto-js");
 const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("../middleware/authorization");
 
