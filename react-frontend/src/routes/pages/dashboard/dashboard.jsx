48a1a0cacce917250372ed7dcaf5ae85c383f3d8
Tharisha Perera <tharisha.perera@cammsgroup.com>
2023-12-07 13:33:23 +0530

    separated dashboards

--- react-frontend/src/routes/pages/dashboard/dashboard.jsx
+++ react-frontend/src/routes/pages/dashboard/dashboard.jsx
@@ -1,31 +1,17 @@
 import React from "react";
-import Notifications from "../../../components/notifications";
-import DashboardCard from "../../../components/dashboard-card";
-import { forestStats, notifications, wildLifeStats } from "../../../data/dummy-data";
+import AdminDashboard from "../../../components/dashboards/admin-dashboard";
+import OfficerDashboard from "../../../components/dashboards/officer-dashboard";
+import PublicDashboard from "../../../components/dashboards/public-dashboard";
 
 const DashboardPage = () => {
+  const userRole = "admin";
+
   return (
-    <div className="text-center">
-      <div className="d-flex">
-        <div className="col-12 col-md-8 px-3">
-          <div className="row d-md-none">
-            <Notifications notifications={notifications} />
-          </div>
-          <div className="row pt-2">
-            <div className="col-12 col-md-6 px-0 py-2 px-md-3 py-md-0">
-              <DashboardCard title="Forest Conservation" data={forestStats} />
-            </div>
-            <div className="col-12 col-md-6 px-0 py-2 px-md-3 py-md-0">
-              <DashboardCard title="Wild Life Conservation" data={wildLifeStats} />
-            </div>
-          </div>
-          <div className="row px-md-3"></div>
-        </div>
-        <div className="col-4 d-none d-md-block p-2">
-          <Notifications notifications={notifications} />
-        </div>
-      </div>
-    </div>
+    <>
+      {userRole === 'admin' && <AdminDashboard />}
+      {userRole === 'officer' && <OfficerDashboard />}
+      {userRole === 'public' && <PublicDashboard />}
+    </>
   );
 };
 
