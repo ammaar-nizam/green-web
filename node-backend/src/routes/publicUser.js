1e70f1e75425ef18d18b5f064796282c8541635b
ammaar-nizam <ammaar.nizam@outlook.com>
2023-12-09 14:01:27 +0530

    Tried user role assignment but failed

--- node-backend/src/routes/publicUser.js
+++ node-backend/src/routes/publicUser.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
 const userController = require('../controllers/userController');
+const authorization = require('../middleware/authorization');
 
 router.get("/:id", userController.getPublicUserById);
 router.get("/", userController.getAllPublicUsers);
-router.patch("/:id", userController.updatePublicUserById);
-router.delete("/:id", userController.deletePublicUserById);
+router.patch("/:id", authorization.verifyToken, userController.updatePublicUserById);
+router.delete("/:id", authorization.verifyToken, userController.deletePublicUserById);
 
 module.exports = router;
