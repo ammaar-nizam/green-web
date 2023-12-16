b23b3786c2dbdd962ae72efe6b9db596e7dc3de0
Tharisha Perera <tharisha.perera@cammsgroup.com>
2023-12-16 21:08:06 +0530

    added jwt support when login

--- react-frontend/src/routes/pages/auth/login.jsx
+++ react-frontend/src/routes/pages/auth/login.jsx
@@ -1,9 +1,19 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { ErrorMessage } from "../../../components/alert-message";
+import { API_URL } from "../../../config/config";
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [userRole, setUserRole] = useState("");
+  const [error, setError] = useState("");
+
+  const roleOptions = [
+    { value: "publicUser", label: "Public User" },
+    { value: "beatOfficer", label: "Beat Officer" },
+    { value: "admin", label: "Admin User" },
+  ];
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
@@ -13,13 +23,46 @@ const LoginPage = () => {
     setPassword(event.target.value);
   };
 
+  const handleRoleChange = (event) => {
+    setUserRole(event.target.value);
+  };
+
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    const apiUrl = "API ENDPOINT URL";
+    // validations
+    if (!userRole) {
+      setError("Please select the user role");
+      return;
+    }
+    if (!email) {
+      setError("Please enter the email address");
+      return;
+    } else {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      let isValid = emailRegex.test(email);
+      if (!isValid) {
+        setError("Please enter a valid email address");
+        return;
+      }
+    }
+    if (!password) {
+      setError("Please enter the password");
+      return;
+    }
+
+    // change endpoint url according to the user role
+    let endpoint = "";
+    if (userRole == "publicUser") {
+      endpoint = API_URL + "/registrations/login";
+    } else if (userRole == "beatOfficer") {
+      endpoint = API_URL + "/beat-officers/login";
+    } else if (userRole == "admin") {
+      endpoint = API_URL + "/admins/login";
+    }
 
     try {
-      const response = await fetch(apiUrl, {
+      const response = await fetch(endpoint, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -30,16 +73,24 @@ const LoginPage = () => {
       if (response.ok) {
         const data = await response.json();
         console.log("Login successful:", data);
-        
-        // Handle successful login, such as updating state or redirecting
-        window.location.href = '/dashboard';
-        
+
+        // store token in session storage
+        localStorage.setItem('accessToken', data.accessToken);
+
+        // redirect to dashboard on successful login
+        window.location.href = "/dashboard";
       } else {
         console.error("Login failed:", response.statusText);
         // Handle login failure, such as displaying an error message
+        setError("User not found. Please check credentials and try again.");
+        return;
       }
     } catch (error) {
       console.error("Error during login:", error);
+      setError("Something went wrong. Please try again later.");
+      return;
+    } finally {
+      setError(null);
     }
   };
 
@@ -48,12 +99,35 @@ const LoginPage = () => {
       <div className="container mt-5">
         <div className="row justify-content-center">
           <div className="col-md-6">
+            {error && (
+              <div className="mt-2">
+                <ErrorMessage message={error} />
+              </div>
+            )}
             <div className="card">
               <div className="card-header d-flex justify-content-center">
                 Login
               </div>
               <div className="card-body">
                 <form onSubmit={handleSubmit}>
+                  <div className="form-group mb-2">
+                    <label htmlFor="user-role">User Role</label>
+                    <select
+                      id="user-role"
+                      value={userRole}
+                      onChange={handleRoleChange}
+                      className="form-control"
+                    >
+                      <option value="" defaultValue>
+                        Select user role...
+                      </option>
+                      {roleOptions.map((role, index) => (
+                        <option key={index} value={role.value}>
+                          {role.label}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
                   <div className="form-group mb-2">
                     <label htmlFor="email">Email</label>
                     <input
@@ -63,7 +137,6 @@ const LoginPage = () => {
                       placeholder="Enter your email"
                       value={email}
                       onChange={handleEmailChange}
-                      required
                     />
                   </div>
                   <div className="form-group mb-2">
@@ -75,7 +148,6 @@ const LoginPage = () => {
                       placeholder="Enter your password"
                       value={password}
                       onChange={handlePasswordChange}
-                      required
                     />
                   </div>
                   <div className="form-group">
