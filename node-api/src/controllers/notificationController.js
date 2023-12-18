ca8c65544d3bc1ce9da6570592478df0ea269ac9
Istina Suresh <istinasuresh28@gmail.com>
2023-12-18 18:06:33 +0530

    added notification service and updated complaints management

--- node-api/src/controllers/notificationController.js
+++ node-api/src/controllers/notificationController.js
@@ -1,24 +1,48 @@
-// const twilio = require('twilio');
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+const models = require('../models');
 
-// // A function to send SMS updates to public users
-// function sendSMS(req, res){
-//     const updateMessage = {
-//         bodyMessage: req.body.bodyMessage, 
-//         toPhoneNumber: req.body.toPhoneNumber
-//     }
-//     const client = new twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
-//     return client.messages
-//         .create({body: updateMessage.bodyMessage, from: '+12058505171', to: updateMessage.toPhoneNumber})
-//         .then((message) => {
-//             res.status(201).json({
-//                 message: message
-//             })
-//         })
-//         .catch((err) =>  {
-//             res.status(500).json({
-//                 error: err
-//             })
-//         });
-// }
+// Initialize nodemailer transporter with Gmail
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_USER,  
+        pass: process.env.EMAIL_PASSWORD 
+    }
+});
+// Function to send notification
+function sendComplaintStatusNotification(publicUserId, newStatus,id) {
+    
+    // Fetch public user's contact information from the database   
+    models.PublicUser.findByPk(publicUserId).then((publicUser) => {
+        if (publicUser) {
+            const contactInfo = {
+                email: publicUser.email,
+                name:publicUser.name,
+            };
 
-// module.exports = {sendSMS}
+            // Send notification using nodemailer 
+            const mailOptions = {
+                from: 'greenwebsdp@gmail.com',
+                to: contactInfo.email,
+                subject: 'Complaint Status Update',
+                text: `Dear ${contactInfo.name},\n\n Complaint status for Complaint ID : ${id} is now updated.\n New Status :  ${newStatus}\n Kindly check your account for more information. \n\n Best Regards,\n Team GreenWeb `
+            };
+
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    console.error('Error sending email:', error);
+                } else {
+                    console.log('Email sent:', info.response);
+                }
+                
+            });
+        }
+    }).catch((err) => {
+        console.error('Error fetching public user information:', err);
+    });
+}
+
+module.exports = {
+    sendComplaintStatusNotification
+};
