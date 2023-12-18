bff85b5a3e4101fd6a6f3a546634563f29d71f5e
Tharisha Perera <tharisha.perera@cammsgroup.com>
2023-12-18 10:34:28 +0530

    added complaints to the public dashboard

--- react-frontend/src/components/dashboards/public-dashboard.jsx
+++ react-frontend/src/components/dashboards/public-dashboard.jsx
@@ -1,10 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Notifications from "../notifications";
 import { complaints, notifications } from "../../data/dummy-data";
+import useAuthToken from "../../hooks/useAuthToken";
+import { API_URL } from "../../config/config";
+import Loader from "../loader";
 
 const PublicDashboard = () => {
+  const [loading, setLoading] = useState(false);
+  const { accessToken } = useAuthToken();
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(API_URL + "/complaints/my-complaints", {
+          headers: {
+            Token: "Bearer " + accessToken,
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch data. Please try again");
+        }
+
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const sortedArray = data && [...data].sort((item1, item2) => item2.id - item1.id);
+  if (loading) {
+    return <Loader/>;
+  }
   return (
     <div className="text-center">
+      {error && <ErrorMessage message={error} />}
       <div className="d-flex">
         <div className="col-12 col-md-8 px-3">
           <div className="row d-md-none">
@@ -14,16 +53,16 @@ const PublicDashboard = () => {
             <div className="card">
               <div className="card-body">
                 <h5 className="card-title mb-4">Recent Complaints</h5>
-                {complaints
+                {sortedArray && sortedArray
                   .map((item, index) => (
                     <div className="card mb-2 text-start p-2" key={index}>
                       <div className="d-flex justify-content-between h-100">
                         <div>
-                          <p className="mb-0">Institution</p>
-                          <p className="mb-0 text-muted">Description</p>
+                          <p className="mb-0">{item.description}</p>
+                          {/* <p className="mb-0 text-muted">{item.description}</p> */}
                         </div>
                         <div className="align-middle">
-                          <p>Status</p>
+                          <p>{item.status}</p>
                         </div>
                       </div>
                     </div>
