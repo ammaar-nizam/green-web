049a1a2516dbe96510404ee0bd30877b344cb6f3
Istina Suresh <istinasuresh28@gmail.com>
2023-12-20 18:52:59 +0530

    adding complaint UIs

--- react-frontend/src/routes/pages/complaints/assigned-complaints/assigned-complaints.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const AssignedComplaints = () => {
-  return <div>AssignedComplaints</div>;
-};
-
-export default AssignedComplaints;
--- react-frontend/src/routes/pages/complaints/my-complaints/my-complaints.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const MyComplaints = () => {
-  return <div>MyComplaints</div>;
-};
-
-export default MyComplaints;
