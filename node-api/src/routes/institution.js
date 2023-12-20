cbb781ceb1aad7893fc8fcf40629749cd81e7971
Istina Suresh <istinasuresh28@gmail.com>
2023-12-20 18:10:07 +0530

    updating institutions

--- node-api/src/routes/institution.js
+++ node-api/src/routes/institution.js
@@ -1,10 +1,11 @@
-const router = require("express").Router();
-const institutionController = require('../controllers/institutionController');
-const authorization = require('../middleware/authorization');
+const router = require("express").Router(); 
+const institutionController = require('../controllers/institutionController'); 
+const authorization = require('../middleware/authorization'); 
+ 
 
-router.post("/", authorization.verifyTokenAndSuperAdmin, institutionController.create);
-router.get("/find/:id", authorization.verifyToken, institutionController.getInstitutionById);
-router.get("/", authorization.verifyToken, institutionController.getAllInstitutions);
-router.get("/stats/:id", authorization.verifyTokenAndAdmin, institutionController.countComplaintsPerEachInstitution);
+router.post("/", authorization.verifyTokenAndSuperAdmin, institutionController.create);  
+router.get("/find/:id", authorization.verifyToken, institutionController.getInstitutionById); 
+router.get("/", authorization.verifyToken, institutionController.getAllInstitutions);  
+router.get("/stats/:id", authorization.verifyTokenAndAdmin, institutionController.countComplaintsPerEachInstitution);  
 
-module.exports = router;
+module.exports = router; 
