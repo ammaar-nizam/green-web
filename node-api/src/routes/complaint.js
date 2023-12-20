0ef788d1f8202f13ff242a5ab83776c975ebbe45
Istina Suresh <istinasuresh28@gmail.com>
2023-12-20 17:19:31 +0530

    adding update route

--- node-api/src/routes/complaint.js
+++ node-api/src/routes/complaint.js
@@ -1,4 +1,9 @@
 const router = require("express").Router(); 
-const complaintController = require('../controllers/complaintController');
+const complaintController = require('../controllers/complaintController');  
+const authorization = require('../middleware/authorization');
+
+router.post("/", authorization.verifyToken, complaintController.upload, complaintController.create);   
+router.get("/my-complaints", authorization.verifyToken, complaintController.getAllComplaintsByPublicUserId);
+router.patch("/:id", authorization.verifyTokenAndAdmin, complaintController.updateComplaintById);  
 
 module.exports = router;  
