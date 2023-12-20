2ae15fedcab5a2a0e8e41b73233fcae05c1019f6
ammaar-nizam <ammaar.nizam@outlook.com>
2023-12-20 18:44:40 +0530

    Added Login for all users and Sign up for the public user

--- react-frontend/src/routes/pages/auth/login.jsx
+++ react-frontend/src/routes/pages/auth/login.jsx
@@ -1,7 +1,187 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { ErrorMessage } from "../../../components/alert-message";
+import { API_URL } from "../../../config/config";
+import Loader from "../../../components/loader";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [userRole, setUserRole] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const roleOptions = [
+    { value: "publicUser", label: "Public User" },
+    { value: "beatOfficer", label: "Beat Officer" },
+    { value: "admin", label: "Admin User" },
+  ];
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleRoleChange = (event) => {
+    setUserRole(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    
+    // validations
+    if (!userRole) {
+      setError("Please select the user role");
+      return;
+    }
+    if (!email) {
+      setError("Please enter the email address");
+      return;
+    } else {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      let isValid = emailRegex.test(email);
+      if (!isValid) {
+        setError("Please enter a valid email address");
+        return;
+      }
+    }
+    if (!password) {
+      setError("Please enter the password");
+      return;
+    }
+
+    // change endpoint url according to the user role
+    let endpoint = "";
+    if (userRole == "publicUser") {
+      endpoint = API_URL + "/registrations/login";
+    } else if (userRole == "beatOfficer") {
+      endpoint = API_URL + "/beat-officers/login";
+    } else if (userRole == "admin" || userRole == "superAdmin") {
+      endpoint = API_URL + "/admins/login";
+    }
+
+    try {
+      setLoading(true);
+      const response = await fetch(endpoint, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Something went wrong. Please try again later.");
+      }
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log("Login successful:", data);
+
+        // store token in session storage
+        localStorage.setItem('accessToken', data.accessToken);
+
+        // redirect to dashboard on successful login
+        window.location.href = "/dashboard";
+      } else {
+        console.error("Login failed:", response.statusText);
+        // Handle login failure, such as displaying an error message
+        setError("User not found. Please check credentials and try again.");
+        return;
+      }
+    } catch (error) {
+      console.error("Error during login:", error);
+      setError(error.message);
+      return;
+    } finally {
+      setError(null);
+      setLoading(false);
+    }
+  };
+  if (loading) {
+    return <Loader />;
+  }
+
+  return (
+    <div>
+      <div className="container mt-5">
+        <div className="row justify-content-center">
+          <div className="col-md-6">
+            {error && (
+              <div className="mt-2">
+                <ErrorMessage message={error} />
+              </div>
+            )}
+            <div className="card">
+              <div className="card-header d-flex justify-content-center">
+                Login
+              </div>
+              <div className="card-body">
+                <form onSubmit={handleSubmit}>
+                  <div className="form-group mb-2">
+                    <label htmlFor="user-role">User Role</label>
+                    <select
+                      id="user-role"
+                      value={userRole}
+                      onChange={handleRoleChange}
+                      className="form-control"
+                    >
+                      <option value="" defaultValue>
+                        Select user role...
+                      </option>
+                      {roleOptions.map((role, index) => (
+                        <option key={index} value={role.value}>
+                          {role.label}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                  <div className="form-group mb-2">
+                    <label htmlFor="email">Email</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="email"
+                      placeholder="Enter your email"
+                      value={email}
+                      onChange={handleEmailChange}
+                    />
+                  </div>
+                  <div className="form-group mb-2">
+                    <label htmlFor="password">Password</label>
+                    <input
+                      type="password"
+                      className="form-control"
+                      id="password"
+                      placeholder="Enter your password"
+                      value={password}
+                      onChange={handlePasswordChange}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <button
+                      type="submit"
+                      className="btn btn-primary mt-3 w-100"
+                    >
+                      Login
+                    </button>
+                  </div>
+                </form>
+                <div className="mt-4 d-flex justify-content-center text-muted">
+                  <p>
+                    Don't have an account? <Link to="/sign-up">Sign-up</Link>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
--- react-frontend/src/routes/pages/auth/signup.jsx
+++ react-frontend/src/routes/pages/auth/signup.jsx
@@ -1,7 +1,244 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  ErrorMessage,
+  SuccessMessage,
+} from "../../../components/alert-message";
+import { API_URL } from "../../../config/config";
+import Loader from "../../../components/loader";
 
 const SignupPage = () => {
-  return <div>SignupPage</div>;
+  const [formData, setFormData] = useState({
+    name: "",
+    nic: "",
+    username: "",
+    email: "",
+    mobile: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    // validation
+    if (!formData.name) {
+      setError("Please enter name");
+      return;
+    } else if (!formData.username) {
+      setError("Please enter a user name");
+      return;
+    } else if (!formData.nic) {
+      setError("Please enter NIC number");
+      return;
+    } else if (!formData.email) {
+      setError("Please enter email address");
+      return;
+    } else if (formData.email) {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      let isValid = emailRegex.test(formData.email);
+      if (!isValid) {
+        setError("Please enter a valid email address");
+        return;
+      }
+    }
+    if (!formData.mobile) {
+      setError("Please enter mobile number");
+      return;
+    } else if (!formData.password) {
+      setError("Please enter a password");
+      return;
+    } else if (!formData.confirmPassword) {
+      setError("Please confirm your password");
+      return;
+    } else if (formData.password !== formData.confirmPassword) {
+      setError("Password and Confirm password does not match");
+      return;
+    }
+
+    try {
+      setLoading(true);
+      const formDataWithoutPWConfirmation = { ...formData };
+      delete formDataWithoutPWConfirmation.confirmPassword;
+
+      const response = await fetch(API_URL + "/registrations/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formDataWithoutPWConfirmation),
+      });
+      if (!response.ok) {
+        console.log(response);
+        throw new Error("Something went wrong. Please try again later.");
+      }
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log("Registration successful:", data);
+
+        setSuccess('Registration successful. You will be redirected to the login page.')
+        
+        // redirect to dashboard on successful registration after 2s
+        setTimeout(function() {          
+          window.location.href = "/";
+        }, 2000)
+      } else {
+        console.error("Registration failed:", response.statusText);
+        setError("Something went wrong. Please try again later.");
+        return;
+      }
+    } catch (error) {
+      console.error("Error during registration:", error);
+      setError(error);
+      return;
+    } finally {
+      setError(null);
+      setLoading(false);
+    }
+  };
+  if (loading) {
+    return <Loader />;
+  }
+
+  return (
+    <div>
+      <div className="container mt-5">
+        <div className="row justify-content-center">
+          <div className="col-md-6">
+            {error && (
+              <div className="mt-2">
+                <ErrorMessage message={error} />
+              </div>
+            )}
+            {success && (
+              <div className="mt-2">
+                <SuccessMessage message={success} />
+              </div>
+            )}
+            <div className="card">
+              <div className="card-header d-flex justify-content-center">
+                Sign-up
+              </div>
+              <div className="card-body">
+                <form onSubmit={handleSubmit}>
+                  <div className="form-group mb-2">
+                    <label htmlFor="name">Name</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="name"
+                      name="name"
+                      placeholder="Enter your name"
+                      value={formData.name}
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <div className="form-group mb-2">
+                    <label htmlFor="username">Username</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="username"
+                      name="username"
+                      placeholder="Enter your username"
+                      value={formData.username}
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <div className="form-group mb-2">
+                    <label htmlFor="nic">NIC Number</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="nic"
+                      name="nic"
+                      placeholder="Enter your NIC number"
+                      value={formData.nic}
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <div className="form-group mb-2">
+                    <label htmlFor="email">Email</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="email"
+                      name="email"
+                      placeholder="Enter your email address"
+                      value={formData.email}
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <div className="form-group mb-2">
+                    <label htmlFor="mobile">Mobile Number</label>
+                    <input
+                      type="test"
+                      className="form-control"
+                      id="mobile"
+                      name="mobile"
+                      placeholder="Enter your mobile number"
+                      value={formData.mobile}
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <div className="form-group mb-2">
+                    <label htmlFor="password">Password</label>
+                    <input
+                      type="password"
+                      className="form-control"
+                      id="password"
+                      name="password"
+                      placeholder="Enter your password"
+                      value={formData.password}
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <div className="form-group mb-2">
+                    <label htmlFor="confirm-password">Confirm Password</label>
+                    <input
+                      type="password"
+                      className="form-control"
+                      id="confirm-password"
+                      name="confirmPassword"
+                      placeholder="Confirm your password"
+                      value={formData.confirmPassword}
+                      onChange={handleChange}
+                    />
+                  </div>
+
+                  <div className="form-group mb-2">
+                    <button
+                      type="submit"
+                      className="btn btn-primary mt-3 w-100"
+                    >
+                      Sign-up
+                    </button>
+                  </div>
+                </form>
+                <div className="mt-4 d-flex justify-content-center text-muted">
+                  <p>
+                    Have an account? <Link to="/">Login</Link>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignupPage;
